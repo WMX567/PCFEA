14b31b95964f0a28f28dc507033dc1ef0cbde660
Mengxi Wu <wmx567@Mengxis-Mac-Studio.local>
2025-07-14 16:43:12 -0700

    test

--- data/PointDA/pcfea/ms1.sh
+++ data/PointDA/pcfea/ms1.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=32GB
-#SBATCH --time=22:00:00
-#SBATCH --gres=gpu:1
-#SBATCH --output=ms1pcfea.out
+#SBATCH --time=00:30:00
+#SBATCH -p nvidia
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=ms3ver13.out
 
 
+source ~/.bashrc
+conda activate py38
 
 
-eval "$(conda shell.bash hook)"
-conda activate pyCLGL
-
-
-
-
-python /scratch1/mengxiwu/PCFEA/SPST_finetune_PCFEA_cls.py --dataroot /scratch1/mengxiwu/PCFEA/data/PointDA/ --src_dataset modelnet --trgt_dataset scannet --seed 1 --model_type pcfea
+python /scratch/mw4355/PCFEA/train_PCFEA_cls.py --dataroot /scratch/mw4355/PCFEA/data/PointDA/ --src_dataset modelnet --trgt_dataset scannet --seed 3 --model_type ver13
